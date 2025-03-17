cc4192698065492a14465c2e0fbffcb82c1cef50
vablings <47798035+vablings@users.noreply.github.com>
2025-03-16 20:52:30 -0500

    clearly some of my finest works here

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,28 @@ use handle::RawHandle;
 
 mod handle;
 
+use aes::cipher::block_padding::Pkcs7;
+use aes::cipher::{BlockEncryptMut, KeyIvInit};
+
+static AES_KEY: &[u8; 16] = b"GIGABYTEPASSWORD";
+static IV: &[u8; 16] = &[0x0; 16];
+
+type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
+
+/// Compute the checksum that the gigabyte driver checks for when issuing an IOCTL.
+fn compute_checksum(input_buffer: &mut Vec<u8>) {
+    let mut checksum = 0u8;
+    for byte in input_buffer.iter() {
+        checksum = checksum.wrapping_add(*byte);
+    }
+    input_buffer.push(!checksum);
+}
+
+/// Encrypt the IOCTL with AES_CBC as the driver expects.
+fn aes_encrypt(input_buffer: &mut [u8]) -> Vec<u8> {
+    Aes128CbcEnc::new(AES_KEY.into(), IV.into()).encrypt_padded_vec_mut::<Pkcs7>(input_buffer)
+}
+
 #[repr(u32)]
 enum IoControlCode {
     MapPhysicalMemory = 0xC350200C,
@@ -107,26 +129,34 @@ impl PhysicalMemory for GdrvDriver {
             ..Default::default()
         };
 
+        let mut input_buffer: Vec<u8> = Vec::new();
+        input_buffer.extend_from_slice(&addr.to_le_bytes());
+        input_buffer.extend_from_slice(&size.to_le_bytes());
+
+        let mut output_buffer = [0u8; 0x10];
+
         unsafe {
             DeviceIoControl(
                 self.handle.handle(),
                 IoControlCode::MapPhysicalMemory as _,
-                Some(&req as *const _ as *const _),
-                mem::size_of::<PhysicalMemoryMappingRequest>() as _,
-                Some(&mut req as *mut _ as *mut _),
-                mem::size_of::<PhysicalMemoryMappingRequest>() as _,
+                Some(input_buffer.as_ptr() as _),
+                input_buffer.len() as _,
+                Some(output_buffer.as_mut_ptr() as _),
+                output_buffer.len() as u32,
                 None,
                 None,
             )
             .map_err(memflow_vdm::Error::Windows)?;
         }
 
+        let virt_addr = u64::from_le_bytes(output_buffer[0..8].try_into().unwrap());
+
         Ok(Box::new(MapPhysicalMemoryResponse {
             phys_addr: addr,
             obj_handle: req.obj_handle,
             section_handle: req.section_handle,
             size,
-            virt_addr: req.virt_addr,
+            virt_addr,
         }))
     }
 
@@ -142,6 +172,9 @@ impl PhysicalMemory for GdrvDriver {
             virt_addr: res.virt_addr(),
             ..Default::default()
         };
+
+        let mut input_buffer: Vec<u8> = Vec::new();
+        input_buffer.extend_from_slice(res.virt_addr().as_bytes_mut());
 
         unsafe {
             DeviceIoControl(
